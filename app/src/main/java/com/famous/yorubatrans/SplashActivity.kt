5d48f34ed82cafc5d2f05421bc73926115fb6736
Success0452 <olatundesuccess54@gmail.com>
2021-06-16 07:25:20 +0100

    update

--- app/src/main/java/com/famous/yorubatrans/SplashActivity.kt
+++ app/src/main/java/com/famous/yorubatrans/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.famous.yorubatrans
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var handler : Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        handler = Handler()
+        handler.postDelayed({
+
+                            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000) //delay 3secs  to open mainActivity
     }
 }
